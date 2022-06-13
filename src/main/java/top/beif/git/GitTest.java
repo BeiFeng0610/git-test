39051a58088cdfe9aa1d7cbd24b834af63c1d26b
Beif610 <beif610@foxmail.com>
2022-06-13 20:05:15 +0800

    third commit

--- src/main/java/top/beif/git/GitTest.java
+++ src/main/java/top/beif/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("GitTest.main");
         System.out.println("GitTest.main2");
+        System.out.println("GitTest.main3");
     }
 }
