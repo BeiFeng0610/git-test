67f119ead1ecd2f6b39f2eb61835a30439aac5ab
Beif610 <beif610@foxmail.com>
2022-06-13 20:00:44 +0800

    first commit

---
+++ src/main/java/top/beif/git/GitTest.java
@@ -0,0 +1,12 @@
+package top.beif.git;
+
+/**
+ * @author 16116
+ * @date 2022-06-13 19:39
+ * @description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("GitTest.main");
+    }
+}
